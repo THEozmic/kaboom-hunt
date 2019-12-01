c67b97ff85578b0313d7a029f534e5b952903b14
Michael Ozoemena <michaelozoemena2@gmail.com>
2019-12-01 10:06:37 +0100

    updates

--- src/components/HomeTab.tsx
+++ src/components/HomeTab.tsx
@@ -13,8 +13,31 @@ import {
 
 import LogOut from "./LogOut";
 
-const HomeTab: React.FC = () => {
+import { useTimer } from "react-timer-hook";
+
+import { RouteComponentProps, withRouter } from "react-router";
+import BackgroundPattern from "./BackgroundPattern";
+
+const HomeTab: React.FC<RouteComponentProps> = ({ history }) => {
   const [hasTicket, changeHasTicket] = useState(false);
+  const [isLoading, changeIsLoading] = useState(false);
+  const [isLoaded, changeIsLoaded] = useState(false);
+  let expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 30);
+
+  const { seconds, minutes } = useTimer({
+    expiryTimestamp,
+    onExpire: () => {} //history.push("/game")
+  });
+
+  const startGame = () => {
+    changeHasTicket(true);
+
+    // Show loading screen...
+    setTimeout(() => {
+      changeIsLoading(true);
+    }, 1000);
+  };
 
   return (
     <IonPage id="speaker-list">
@@ -37,91 +60,149 @@ const HomeTab: React.FC = () => {
       </IonHeader>
 
       <IonContent>
-        <div
-          style={{
-            backgroundColor: "var(--ion-color-light)",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
+        {isLoading ? (
           <div
             style={{
-              fontWeight: "bold",
-              fontSize: "2em",
-              padding: "20px",
-              color: "var(--ion-color-primary)"
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100%"
             }}
           >
-            <div>
-              <div className="heading">
-                <div
-                  style={{
-                    color: "var(--ion-color-tertiary-contrast)"
-                  }}
-                >
-                  Win Cash Prizes
+            <div
+              style={{
+                color: "var(--ion-color-primary)",
+                fontSize: "2em",
+                fontWeight: "bold"
+              }}
+            >
+              Game Starts In
+            </div>
+            <p
+              style={{
+                fontWeight: "bold",
+                fontSize: "3em",
+                margin: "0",
+                color: "var(--ion-color-tertiary-contrast)"
+              }}
+            >
+              {`${minutes}:${seconds}`}
+            </p>
+            <BackgroundPattern />
+          </div>
+        ) : (
+          <div
+            style={{
+              backgroundColor: "var(--ion-color-light)",
+              height: "100%",
+              display: "flex",
+              flexDirection: "column"
+            }}
+          >
+            <div
+              style={{
+                fontWeight: "bold",
+                fontSize: "2em",
+                padding: "20px",
+                color: "var(--ion-color-primary)"
+              }}
+            >
+              <div>
+                <div className="heading">
+                  <div
+                    style={{
+                      color: "var(--ion-color-tertiary-contrast)"
+                    }}
+                  >
+                    Win Cash Prizes
+                  </div>
+                  <div>In Weekly</div>
+                  <div>Scavenger Hunts</div>
                 </div>
-                <div>In Weekly</div>
-                <div>Scavenger Hunts</div>
               </div>
             </div>
-          </div>
 
-          <div
-            style={{
-              padding: "10px 20px",
-              backgroundColor: "var(--ion-color-primary-contrast)"
-            }}
-          >
-            <IonText color="primary">
-              <h2
-                style={{
-                  fontWeight: "bolder",
-                  textAlign: "center",
-                  padding: "0 30px"
-                }}
-              >
-                Next Game <br />
-                At Freedom Park, Lagos Island
-              </h2>
+            <div
+              style={{
+                padding: "10px 20px",
+                backgroundColor: "var(--ion-color-primary-contrast)"
+              }}
+            >
+              <IonText color="primary">
+                <h2
+                  style={{
+                    fontWeight: "bolder",
+                    textAlign: "center",
+                    padding: "0 30px"
+                  }}
+                >
+                  Next Game
+                </h2>
+              </IonText>
               <p
                 style={{
-                  textAlign: "center",
-                  fontWeight: "bold",
-                  fontSize: "2em",
-                  margin: "15px",
                   color: "var(--ion-color-tertiary-contrast)"
                 }}
               >
-                12 PM - 5 PM
+                <h2
+                  style={{
+                    fontWeight: "bolder",
+                    textAlign: "center",
+                    padding: "0 30px"
+                  }}
+                >
+                  THIS FRIDAY
+                </h2>
               </p>
-            </IonText>
-          </div>
+              <IonText color="primary">
+                <h2
+                  style={{
+                    fontWeight: "bolder",
+                    textAlign: "center",
+                    padding: "0 30px"
+                  }}
+                >
+                  At Freedom Park,
+                  <br />
+                  Lagos Island
+                </h2>
+                <p
+                  style={{
+                    textAlign: "center",
+                    fontWeight: "bold",
+                    fontSize: "1.3em",
+                    margin: "15px",
+                    color: "var(--ion-color-tertiary-contrast)"
+                  }}
+                >
+                  12 PM - 5 PM
+                </p>
+              </IonText>
+            </div>
 
-          <div
-            style={{
-              padding: "20px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-              fontSize: "1.2em",
-              fontWeight: "bold"
-            }}
-          >
-            {hasTicket ? (
-              <IonText color="primary">You have a ticket!</IonText>
-            ) : (
-              <IonButton onClick={() => changeHasTicket(true)}>
-                BUY A TICKET
-              </IonButton>
-            )}
+            <div
+              style={{
+                padding: "20px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                fontSize: "1.2em",
+                fontWeight: "bold"
+              }}
+            >
+              {hasTicket ? (
+                <IonText color="primary">You have a ticket!</IonText>
+              ) : (
+                <IonButton onClick={() => startGame()}>BUY A TICKET</IonButton>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </IonContent>
     </IonPage>
   );
 };
 
-export default HomeTab;
+export default withRouter(HomeTab);
